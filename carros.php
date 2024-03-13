b3612cc6ff47e4edd7da62de31556bf5f1b6aaf2
Arthur-Prates <arthurprateslima@gmail.com>
2024-03-13 15:58:10 -0300

    att

--- carros.php
+++ carros.php
@@ -5,6 +5,7 @@
         <th scope="col">Proprietário</th>
         <th scope="col">Carro</th>
         <th scope="col">Valor</th>
+        <th scope="col">Ação</th>
     </tr>
     </thead>
     <tbody>
