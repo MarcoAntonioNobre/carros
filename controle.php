278d114a8ca3b106326cb66cc258fa4df57c3c0e
Arthur-Prates <arthurprateslima@gmail.com>
2024-03-13 14:39:34 -0300

    novos arquivos

---
+++ controle.php
@@ -0,0 +1,27 @@
+<?php
+
+include_once("config/constantes.php");
+include_once("config/conexao.php");
+include_once("func/funcoes.php");
+$controle = filter_input(INPUT_POST, 'controle', FILTER_SANITIZE_STRING);
+if (!empty($controle) && isset($controle)) {
+    switch ($controle) {
+        case 'Algo':
+            include_once('Direção.php');
+            break;
+
+    }
+
+} else {
+    ?>
+    <div style="display: flex;justify-content: center;align-items: center; min-height: 95vh !important;">
+        <h1>Página Vazia, Retorne. </h1><sup>Error 404</sup>
+        <img src="./img/vazio.gif" alt="ERROR 404">
+    </div>
+    <?php
+}
+
+//$acao = filter_input(INPUT_POST, 'ação', FILTER_SANITIZE_STRING);
+//$acaoId = filter_input(INPUT_POST, 'acaoId', FILTER_SANITIZE_NUMBER_INT);
+//$controleGet = filter_input(INPUT_GET, 'controleGet', FILTER_SANITIZE_STRING);
+
---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+include_once("config/constantes.php");
+include_once("config/conexao.php");
+include_once("func/funcoes.php");
+?>
+
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <title>Dashboard</title>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" href="./css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"/>
+    <link rel="stylesheet" type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/7.0.96/css/materialdesignicons.min.css">
+</head>
+
+<body>
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+        crossorigin="anonymous"></script>
+<script src="./js/script.js"></script>
+
+</body>
+
+</html>
+
