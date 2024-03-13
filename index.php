8c81dbaee26061f3c5b473df150e28574ac901da
Arthur-Prates <arthurprateslima@gmail.com>
2024-03-13 15:08:02 -0300

    novos arquivos

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ include_once("func/funcoes.php");
 </head>
 
 <body>
-
+<button onclick=" carregarConteudo('carros')">clica</button>
 
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
         integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
@@ -28,6 +28,11 @@ include_once("func/funcoes.php");
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
         integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
         crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/0.9.0/jquery.mask.min.js"
+        integrity="sha512-oJCa6FS2+zO3EitUSj+xeiEN9UTr+AjqlBZO58OPadb2RfqwxHpjTU8ckIC8F4nKvom7iru2s8Jwdo+Z8zm0Vg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="./js/script.js"></script>
 
 </body>
--- js/script.js
+++ js/script.js
@@ -12,3 +12,18 @@ var options = {
 
 $('.telefoneBR').mask('(00) 0 0000-0000', options);
 
+function carregarConteudo(controle){
+    fetch('controle.php', {
+        method: 'POST', headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }, body: 'controle=' + encodeURIComponent(controle),
+    })
+        .then(response => response.text())
+        .then(data => {
+
+            document.getElementById('show').innerHTML= data;
+        })
+        .catch(error => {
+            console.error('Erro na requisição:', error);
+        });
+}
