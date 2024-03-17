0e8a8a8d737ddd09d9ace42432385187cbe6e3d7
Larc Oliveira <146396747+cclar13@users.noreply.github.com>
2024-03-17 18:17:48 -0300

    login com att graficas

--- footer.php
+++ footer.php
@@ -1,14 +1,66 @@
+<div class="container-fluid align-items-end">
+    <div class="row mt-5">
+        <div class="corzinha mt-5 pt-5 ">
+            <section class="ftco-section">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-md-6 text-center corpadrao">
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <footer class="footer-02">
+                <div class="container">
+                    <div class="row text-center ">
+                        <div class="col-md-12 col-lg-12">
+                            <div class="row ">
+                                <div class="col-md-12 col-lg-12 mb-md-0 mb-4 corpadrao ">
+                                    <h2 class="footer-heading"><a href="#"
+                                                                  class="logo"><strong>streetcar.com</strong></a></h2>
+                                    <p class="corpadrao">Os melhores carros do senai MG!</p>
+                                </div>
+                            </div>
+                        </div>
 
-<!--<footer class="text-center text-lg-start" id="footer" style="background-color: rgb(0, 0, 0);">-->
-<!--    <div class="container d-flex justify-content-center py-5">-->
-<!--        <a href="https://github.com/MarcoAntonioNobre/carros" target="_blank">-->
-<!--            <i class="bi bi-github icones teste fs-1"></i>-->
-<!--        </a>-->
-<!--    </div>-->
-<!---->
-<!--     Copyright -->
-<!--    <div class="text-center text-white p-3" style="background-color: rgba(0, 0, 0, 0.2);">-->
-<!--        © StreetCar, 2024. Todos os direitos reservados.-->
-<!--    </div>-->
-<!--     Copyright -->
-<!--</footer>-->
+                    </div>
+                    <div class="row partner-wrap mt-5 ">
+                        <div class="col-md-12">
+                            <div class="row align-items-center corpadrao">
+                                <div class="col">
+                                    <h3 class="mb-0">Nossos Parceiros:</h3>
+                                </div>
+                                <div class="col-md-9">
+                                    <p class="partner-name mb-0 corpadrao ">
+                                        <a href="#"><span class="ion-logo-ionic mr-2"></span>OSAKA |</a>
+                                        <a href="#"><span class="ion-logo-ionic mr-2"></span>BUGATTI |</a>
+                                        <a href="#"><span class="ion-logo-ionic mr-2"></span>MCLAREN |</a>
+                                        <a href="#"><span class="ion-logo-ionic mr-2"></span>PORSCHE</a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mt-5 ">
+                        <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top text-light">
+                            <p>© StreetCar  <script>document.write(new Date().getFullYear());</script>  Todos os direitos reservados.</p>
+                            <ul class="list-unstyled wrapper ">
+                                <li class="icon twitter">
+                                    <span class="tooltip">Twitter</span>
+                                    <span><i class="fab fa-x-twitter"></i></span>
+                                </li>
+                                <li class="icon instagram">
+                                    <span class="tooltip">Instagram</span>
+                                    <span><i class="fab fa-instagram"></i></span>
+                                </li>
+                                <li class="icon youtube">
+                                    <span class="tooltip">Spotify</span>
+                                    <span><i class="fab fa-spotify"></i></span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -9,7 +9,7 @@ include_once("func/funcoes.php");
 <html lang="pt-br">
 
 <head>
-    <title>Dashboard</title>
+    <title>AREA DE LOGIN</title>
     <meta charset="utf-8" />
     <link rel="stylesheet" href="./css/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
@@ -17,25 +17,25 @@ include_once("func/funcoes.php");
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/7.0.96/css/materialdesignicons.min.css">
     <link rel="stylesheet" href="./css/index.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css'>
 </head>
 
-<body class="bodylogin" style="background-image: url('./img/funlogin3.jpg');background-size: cover;background-repeat: no-repeat;width: 100%;height: 100%;">
+<body class="bodylogin text-white" style="background-image: url('./img/funlogin3.jpg');background-size: cover;background-repeat: no-repeat;width: 100%;height: 100%;">
     <nav class="navbar bg-dark " data-bs-theme="dark">
-        <div class="container-fluid">
-            <a class="navbar-brand text-black" href="#">
-                <img src="./img/icon.jpg" alt="Logo" width="30px" class="d-inline-block align-text-top">
-                Carro
+        <div class="container-fluid " >
+            <a class="navbar-brand text-white" href="index.php" style="font-family:monospace;">
+                StreetCar
             </a>
         </div>
     </nav>
-    <div class="container mt-5 ">
+    <div class="container mt-5">
         <div class="row justify-content-md-center">
-            <div class="col-md-4">
+            <div class="col-md-4 ">
                 <div class="wrapper">
                     <form action="login.php" method="post" name="frmLogin" id="frmLogin">
                         <h1>LOGIN</h1>
                         <div class="input-box">
-                            <input type="text" name="cpf" id="cpf" placeholder="Cpf">
+                            <input type="text" name="cpf" id="cpf" placeholder="Cpf" autocomplete="off" oninput="mascaraCPF(this)">
                             <i class="bi bi-person-bounding-box"></i>
                         </div>
                         <div class="input-box">
@@ -50,39 +50,49 @@ include_once("func/funcoes.php");
             </div>
         </div>
     </div>
+    <footer class="mt-5">
+        <div class="card text-center bg-dark text-white">
 
-    <!--<button onclick=" carregarConteudo('carros')">clica</button>-->
-
+            <div class="card-body">
+                <ul class="list-unstyled wrapper1 ">
+                    <li class="icon twitter">
+                        <span class="tooltip">Twitter</span>
+                        <span><i class="fab fa-x-twitter"></i></span>
+                    </li>
+                    <li class="icon instagram">
+                        <span class="tooltip">Instagram</span>
+                        <span><i class="fab fa-instagram"></i></span>
+                    </li>
+                    <li class="icon youtube">
+                        <span class="tooltip">Spotify</span>
+                        <span><i class="fab fa-spotify"></i></span>
+                    </li>
+                </ul>
+            </div>
+            <div class="card-footer text-white">
+                <p>© StreetCar <script>
+                        document.write(new Date().getFullYear());
+                    </script> Todos os direitos reservados.</p>
+            </div>
+        </div>
+    </footer>
+    <script src="./js/formatacoes.js"></script>
+    <script src="./js/js.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/0.9.0/jquery.mask.min.js" integrity="sha512-oJCa6FS2+zO3EitUSj+xeiEN9UTr+AjqlBZO58OPadb2RfqwxHpjTU8ckIC8F4nKvom7iru2s8Jwdo+Z8zm0Vg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     </script>
-    </head>
-    <script src="./js/formatacoes.js"></script>
-    <script src="./js/js.js"></script>
-    <script src="http://www.geradorcpf.com/scripts.js"></script>
-    <script src="http://www.geradorcpf.com/jquery-1.2.6.pack.js"></script>
-    <script src="http://www.geradorcpf.com/jquery.maskedinput-1.1.4.pack.js"></script>
-
-    </script>
-
-    <script type="text/javascript">
+   
+    <script>
+        // $('#cpf').mask('000.000.000-00', {reverse: true});
         $(document).ready(function() {
-            $(".cpf").mask("999.999.999-99");
-
-            $("#cpf").blur(function() {
-                if ($("#cpf").val() == '') {
-                    $("#saida").html("Informe um CPF");
-                    return false;
-                }
-                if (validarCPF($("#cpf").val())) {
-                    $(".cpf").css('border-color', 'limegreen');
-                } else {
-                    $(".cpf").css('border-color', 'red');
-                }
+            var $cpf = $("#cpf");
+            $cpf.mask('000.000.000-00', {
+                reverse: false
             });
         });
+       
     </script>
 </body>
 
